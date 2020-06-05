ab2f58ad69e0d2470bad936f8f8aeed197c1f1c4
iarafat <miyasinarafat@gmail.com>
2020-06-05 19:33:50 +0600

    updated items list

--- components/Items.vue
+++ components/Items.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <ul class="mt-10 mb-40 ml-20 list-disc">
-      <li v-for="item in items" :key="item.id">
-        {{ item.title }}
+    <ul class="mt-10 mb-40 ml-20 mr-20 list-none">
+      <li class="mb-10 broder-solid border-2 border-gray-300 p-10" v-for="item in items" :key="item.id">
+        <div class="score">{{ item.score }}</div>
+        <div class="title">{{ item.title }}</div>
+        <div class="details">by {{ item.by }} {{ item.time }}</div>
+        <div class="comments">{{ item.descendants }} comments</div>
       </li>
     </ul>
   </div>
