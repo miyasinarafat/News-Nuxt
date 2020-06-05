ab8e8cb125b9b4356ab334c29910bec8a63b3f7f
iarafat <miyasinarafat@gmail.com>
2020-06-05 21:51:27 +0600

    added user page

--- components/Items.vue
+++ components/Items.vue
@@ -4,7 +4,7 @@
       <li class="mb-10 broder-solid border-2 border-gray-300 p-10" v-for="item in items" :key="item.id">
         <div class="score">Score: {{ item.score }}</div>
         <div class="title">Title: <a target="_blank" class="hover:text-blue-500" :href="item.url">{{ item.title }}</a></div>
-        <div class="details">Details: by {{ item.by }} {{ item.time | timeSince }}</div>
+        <div class="details">Details: by <nuxt-link class="hover:text-blue-500 text-blue-700" :to="`/user/${item.by}`">{{ item.by }},</nuxt-link> {{ item.time | timeSince }}</div>
         <div class="comments" v-if="item.descendants">Comments: {{ item.descendants }}</div>
         <div class="link" v-if="item.url">Link: <a target="_blank" class="hover:text-blue-500 text-blue-700" :href="item.url">{{ item.url | hostname }}</a></div>
       </li>
---
+++ pages/user/_id.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="ml-20 mr-20">
+      <h1 class="text-3xl">{{user.id}} has been a user {{ user.created | timeSince}}</h1>
+    </div>
+</template>
+
+<script>
+  import axios from "@/plugins/axios";
+    export default {
+      async asyncData({route}) {
+        const response = await axios.get(`/user/${route.params.id}.json`)
+        return {user: response.data}
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
